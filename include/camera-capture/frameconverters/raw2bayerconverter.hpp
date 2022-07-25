aec79e9aee9e252efc2f6d8310075690be80605b
Anna Panfil <apanfil@internships.antmicro.com>
2022-07-25 08:29:18 +0200

    [#35769] frameconverters: raw2bayer: Add bayer frame support
    Signed-off-by: Anna Panfil <apanfil@internships.antmicro.com>

--- include/camera-capture/frameconverters/raw2bayerconverter.hpp
+++ include/camera-capture/frameconverters/raw2bayerconverter.hpp
@@ -14,7 +14,7 @@ class Raw2BayerConverter : public FrameConverter
 {
 public:
     /**
-     * Constructor
+     * Constructor for Bayer converter
      *
      * @param code OpenCV's color space conversion code (see
      * https://docs.opencv.org/4.5.2/d8/d01/group__imgproc__color__conversions.html#ga57261f12fccf872a2b2d66daf29d5bd0).
@@ -23,12 +23,28 @@ public:
      * @param nChannels Number of channels in the destination image; if the parameter is 0, the number of the channels
      * is derived automatically from raw matrix and code.
      */
-    Raw2BayerConverter(int code, int destMatType = CV_8UC3, int nChannels = 0);
+    Raw2BayerConverter(int code, int destMatType = CV_8UC3, int nChannels = 0)
+        : code(code), destMatType(destMatType), nChannels(nChannels)
+    {
+    }
 
     /**
-     * Demosaicing
+     * Perform demosaicing
+     *
+     * @param src Matrix to convert
      */
     cv::Mat convert(cv::Mat src) override;
+
+    /**
+     * Convert Bayer to RGB
+     *
+     * @param src Frame to convert
+     * @param src_dtype OpenCV's datatype for source matrix (see
+     * https://docs.opencv.org/3.4/d1/d1b/group__core__hal__interface.html)
+     * @param width Source frame width in pixels
+     * @param height Source frame height in pixels
+     */
+    cv::Mat convert(std::shared_ptr<MMapBuffer> src, int src_dtype, int width, int height) override;
 
 private:
     int code;        ///< OpenCV's Color space conversion code (see: constructor)
--- src/frameconverters/raw2bayerconverter.cpp
+++ src/frameconverters/raw2bayerconverter.cpp
@@ -1,14 +1,15 @@
 #include "camera-capture/frameconverters/raw2bayerconverter.hpp"
 
-Raw2BayerConverter::Raw2BayerConverter(int code, int destMatType, int nChannels)
-    : code(code), destMatType(destMatType), nChannels(nChannels)
-{
-}
-
 cv::Mat Raw2BayerConverter::convert(cv::Mat src)
 {
     // std::cout << "Bayer\n";
     cv::Mat processed_frame = cv::Mat(src.rows, src.cols, destMatType);
     cv::demosaicing(src, processed_frame, code, nChannels);
     return processed_frame;
+}
+
+cv::Mat Raw2BayerConverter::convert(std::shared_ptr<MMapBuffer> src, int src_dtype, int width, int height)
+{
+    cv::Mat raw_frame = cv::Mat(height, width, src_dtype, src->start);
+    return convert(raw_frame);
 }
