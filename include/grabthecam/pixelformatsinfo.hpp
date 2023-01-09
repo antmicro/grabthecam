fbe7d453d2dfd2e2186482524ae286653f9f11b0
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-01-09 12:57:08 +0100

    [#40025] pixelformatsinfo: Fixed documentation comment
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- include/grabthecam/pixelformatsinfo.hpp
+++ include/grabthecam/pixelformatsinfo.hpp
@@ -10,6 +10,7 @@
 namespace grabthecam
 {
 
+/// Information about converters and input formats assigned to pixel formats
 static std::unordered_map<unsigned int, std::function<std::shared_ptr<FrameConverter>()>> formats_info = {
     {V4L2_PIX_FMT_YYUV, [] { return std::make_shared<Yuv2BGRConverter>(cv::COLOR_YUV2BGR_YUY2); }},
     {V4L2_PIX_FMT_YUYV, [] { return std::make_shared<Yuv2BGRConverter>(cv::COLOR_YUV2BGR_YUY2); }},
@@ -19,13 +20,6 @@ static std::unordered_map<unsigned int, std::function<std::shared_ptr<FrameConve
     {V4L2_PIX_FMT_SRGGB12,
      [] { return std::make_shared<Bayer2BGRConverter>(cv::COLOR_BayerRG2BGR, CV_16UC1, CV_16UC3); }},
     {V4L2_PIX_FMT_SRGGB10,
-     [] { return std::make_shared<Bayer2BGRConverter>(cv::COLOR_BayerRG2BGR, CV_16UC1, CV_16UC3); }}}; ///< Information
-                                                                                                       ///< about
-                                                                                                       ///< converters
-                                                                                                       ///< and input
-                                                                                                       ///< formats
-                                                                                                       ///< assigned to
-                                                                                                       ///< pixel
-                                                                                                       ///< formats
+     [] { return std::make_shared<Bayer2BGRConverter>(cv::COLOR_BayerRG2BGR, CV_16UC1, CV_16UC3); }}};
 
 };
