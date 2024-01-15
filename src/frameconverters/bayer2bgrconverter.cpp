b45d095c3559722a11f66f9699b0c4a7a8733e7c
Jan Bylicki <jbylicki@hotmail.com>
2024-01-15 17:00:48 +0100

    bayer2bgrconverter: Added 10 and 12 bit support for the converter
    Co-authored-by: DiegoBalagane <kacpermln@wp.pl>
    Co-authored-by: Reinie78 <dominik.pytka777@gmail.com>

--- src/frameconverters/bayer2bgrconverter.cpp
+++ src/frameconverters/bayer2bgrconverter.cpp
@@ -8,7 +8,15 @@ namespace grabthecam
 cv::Mat Bayer2BGRConverter::convert(cv::Mat src)
 {
     cv::Mat processed_frame = cv::Mat(src.rows, src.cols, dest_mat_type);
-    cv::demosaicing(src, processed_frame, code, nchannels);
+    if (this->input_format == CV_16UC1)
+    {
+        src.convertTo(src, CV_8UC1);
+        cv::demosaicing(src, processed_frame, code, nchannels);
+    }
+    else
+    {
+        cv::demosaicing(src, processed_frame, code, nchannels);
+    }
     return processed_frame;
 }
 
